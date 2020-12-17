129d1869d49c4fd74bc0a33d82aa3942238bec34
Lucas Joyce <52796033+lucas-Joyce@users.noreply.github.com>
2020-12-17 18:35:44 +0000

    Day11 - Dynamic Favicon

---
+++ asset/js/dayOf11Script.js
@@ -0,0 +1,7 @@
+const favicon = document.getElementById("favicon");
+function toRed() {
+    favicon.setAttribute("href", "./img/HE.png");
+}
+function toBlue() {
+    favicon.setAttribute("href", "./img/WING.png");
+}
